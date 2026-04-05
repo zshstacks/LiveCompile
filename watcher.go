0944add63256fa2a4ca62dde17ebe95f21bced67
zshstacks <uns4d123@gmail.com>
2026-04-06 00:07:02 +0300

    ADDED: watcher

---
+++ watcher.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io/fs"
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func watcher() {
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer w.Close()
+
+	//walk and add every dir to the watcher
+	filepath.WalkDir(".", func(s string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir(){
+			w.Add(s)
+		}
+
+		return nil
+	})
+
+	log.Println("Watching for changes...")
+
+	//block and lister fore events
+	for event := range w.Events {
+	
+		//filter .go files 
+		if !strings.HasSuffix(event.Name, ".go") {
+			continue
+		}
+		if event.Op&fsnotify.Write == fsnotify.Write {
+			log.Println("File changed: ", event.Name)
+			builder()
+		}
+	}
+}
